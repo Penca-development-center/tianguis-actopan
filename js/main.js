89032a98b80111dcbc24439476e1f5c48d193954
peca-develpment <feliph_ls@hotmail.com>
2020-02-26 16:50:54 -0600

    Primer commit del proyecto.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var menu_fix = document.getElementById('t-menu');
+var icono_aparece = document.getElementById('icono-aparece');
+var menu_disp = document.getElementById('menu-trigger');
+var full_menu = document.getElementById('full-menu');
+var menu_bajo = document.getElementById('icon-bottom');
+const ANCHO_VENTANA = window.innerHeight;
+const LARGO_VENTANA = window.innerWidth;
+const ANCHO_MENU = menu_fix.clientHeight;
+const ANCHO_ICONO = icono_aparece.clientHeight;
+
+// Evento scroll, que permitira la navegacion en pantalla del algunos elementos de la página
+window.addEventListener('scroll', ev => {
+    var scrollPosition = window.scrollY;
+   
+    if(scrollPosition > ANCHO_ICONO){
+        console.log("Aqui tiene que aparecer: " + icono_aparece.clientHeight);
+    } else {
+        console.log('aqui ya no');
+    }
+});
+
+//Agregando el menu de patntalla completa
+
+//Agregando la funcion de boton slider
+
+// Aqui va lo de three js.
+
+// Acomodar todo con webpack
