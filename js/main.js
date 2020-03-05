e7baae4286d45546f3a7b2c741152e13894cd587
peca-develpment <feliph_ls@hotmail.com>
2020-03-05 15:45:46 -0600

    Agregando compatibilidad con otros navegadores

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-
 /*Variables*/
+
+/* Elementnos de la página */ 
 var menu_fix = document.getElementById('t-menu');
 var icono_aparece = document.getElementById('icono-aparece');
 var menu_disp = document.getElementById('menu-trigger');
@@ -7,40 +8,40 @@ var full_menu = document.getElementById('full-menu');
 var menu_bajo = document.getElementById('icon-bottom');
 var menu_close = document.getElementById('menu-close');
 
-//Constantes
+/* Iniicialización de las variables */
+var camera, scene; // Esto es para la libreria three.ja
+var scrollPosition; // Para obtener la posicion del scroll en el eje y.
+
+/* Constantes */
 const ANCHO_VENTANA = window.innerHeight;
 const LARGO_VENTANA = window.innerWidth;
 const ANCHO_MENU = menu_fix.clientHeight;
 const ANCHO_ICONO = icono_aparece.clientHeight;
 
-// Evento scroll, que permitira la navegacion en pantalla del algunos elementos de la página
-window.addEventListener( 'scroll', () => 
-{
+/* Evento scroll, que permitira la navegacion en pantalla del algunos elementos de la página */
+window.addEventListener( 'scroll', () => {
 	var scrollPosition = window.scrollY;
    
-	if(scrollPosition > ANCHO_ICONO)
-	{
+	if(scrollPosition > ANCHO_ICONO){
 		console.log( `Aqui tiene que aparecer: ${icono_aparece.clientHeight}` );
 		console.log(`Posicion: ${scrollPosition}, Ancho del elemento: ${ANCHO_ICONO}`);
 		scrollPosition === ANCHO_ICONO ? console.log("Aqui es igual") : console.log("...");
-	} 
-	else 
-	{
-    console.log( 'aqui ya no' );
+	} else {
+  	console.log( 'aqui ya no' );
 	}
 	
 });
 
-//Agregando el menu de patntalla completa
-menu_disp.addEventListener('click', ev => {
-	console.log(`abre`);
+/* Agregando el menu de patntalla completa */
+
+/* Boton que muestra el menú */ 
+menu_disp.addEventListener('click', () => {
 	full_menu.style.opacity = 1;
 	full_menu.style.zIndex = 70;
-	full_menu.style.animation='aparece .5s .1s ease linear';
 });
 
+/* Boton que oculta el menú */ 
 menu_close.addEventListener('click', () => {
-	console.log(`cierrra`);
 	full_menu.style.opacity = 0;
 	full_menu.style.zIndex = 50;
 });
